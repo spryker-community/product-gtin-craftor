ab333876fa0220c363a42d65d4c5a5f6a73628c7
Alexander Ratajczak <alexander.ratajczak@diva-e.com>
2023-09-13 13:51:28 +0200

    added facade

--- src/SprykerCommunity/Client/UpcDatabase/Api/UpcDatabaseApiClientInterface.php
+++ src/SprykerCommunity/Client/UpcDatabase/Api/UpcDatabaseApiClientInterface.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * MIT License
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
 namespace Pyz\Client\UpcDatabase\Api;
 
 use Generated\Shared\Transfer\UpcRequestTransfer;
--- src/SprykerCommunity/Zed/ProductGtinCraftor/Business/ProductGtinCraftorFacadeInterface.php
+++ src/SprykerCommunity/Zed/ProductGtinCraftor/Business/ProductGtinCraftorFacadeInterface.php
@@ -7,6 +7,40 @@
 
 namespace SprykerCommunity\Zed\ProductGtinCraftor\Business;
 
+use Generated\Shared\Transfer\ProductAbstractTransfer;
+
 interface ProductGtinCraftorFacadeInterface
 {
+    /**
+     * Expands Product with GTIN Data
+     *
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\ProductAbstractTransfer $productAbstractTransfer
+     *
+     * @return \Generated\Shared\Transfer\ProductAbstractTransfer
+     */
+    public function expandProductAbstractWithGtinData(ProductAbstractTransfer $productAbstractTransfer): ProductAbstractTransfer;
+
+    /**
+     * Expands Product with OpenAI Data
+     *
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\ProductAbstractTransfer $productAbstractTransfer
+     *
+     * @return \Generated\Shared\Transfer\ProductAbstractTransfer
+     */
+    public function expandProductAbstractWithOpenAIData(ProductAbstractTransfer $productAbstractTransfer): ProductAbstractTransfer;
+
+    /**
+     * Expands Product with LMIV Data
+     *
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\ProductAbstractTransfer $productAbstractTransfer
+     *
+     * @return \Generated\Shared\Transfer\ProductAbstractTransfer
+     */
+    public function expandProductAbsractWithLMIVData(ProductAbstractTransfer $productAbstractTransfer): ProductAbstractTransfer;
 }
